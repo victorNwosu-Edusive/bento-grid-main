2ba9de28405b40d8a26ca7dcee7d637e0f9e2626
Victor Nwosu <nwosuuvictor@gmail.com>
2024-11-10 01:39:44 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html", "./public/index.html"],
+  theme: {
+    fontFamily:{
+      primary:['DM Sans']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
